af55ee0f38aa0729e61044c9a737d49098279304
Shik2003 <100432857+Shik2003@users.noreply.github.com>
2022-02-26 19:34:43 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 extends MainHyperlink {
+	
+	public static void main(String[] args) throws Exception {
+		Test1 t1 = new Test1();
+		
+		t1.setup();
+         t1.testweb10();
+          t1.testweb11();
+           t1.testweb12();
+           t1.testweb13();
+           t1.tearDown();
+	}
+
+}
