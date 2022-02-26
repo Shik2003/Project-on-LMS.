5db7ec60f5ccee12495c34033fbadceb4f4957ae
Shik2003 <100432857+Shik2003@users.noreply.github.com>
2022-02-26 19:33:26 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test extends Login{
+	public static void main(String[] args) throws Exception {
+		Test t = new Test();
+		t.setUp();
+		t.testweb();
+		t.testweb1();
+		t.testweb2();
+		t.testweb3();
+		t.testweb4();
+		t.testweb5();
+		t.testweb6();
+		t.testweb7();
+		t.testweb8();
+		t.testweb9();		
+		t.tearDown();	
+		
+		
+		
+	
+	}	
+	}
+	
+	
+
+
